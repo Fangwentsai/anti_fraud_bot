5efb4371f56c3794bff5289c04b44fdf2564ab76
Fangwentsai <fangwentsai14@gmail.com>
2025-05-17 12:23:27 +0800

    Fix: Import PostbackAction for potato game

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage, FlexSendMessage,
-    QuickReply, QuickReplyButton, MessageAction, PostbackEvent,
+    QuickReply, QuickReplyButton, MessageAction, PostbackEvent, PostbackAction,
     BubbleContainer, BoxComponent, ButtonComponent, TextComponent,
     CarouselContainer, URIAction, SeparatorComponent
 )
